7c6de3c144158f056f2a16dfa6854f29db8614d3
QuocCuong <trquoccuong@gmail.com>
2015-11-17 15:53:23 +0700

    Convert logic upload file roxy fileman 'need testing'

---
+++ features/upload/feature.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    title: __('m_upload_backend_module_title'),
+    author: 'Nguyen Van Thanh',
+    version: '0.1.1',
+    description: __('m_upload_backend_module_desc'),
+    permissions: []
+};
+
