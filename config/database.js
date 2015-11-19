d75e0483b4dff83747c4d38edd3b7922bfc494b5
thangnv <thangtyt@gmail.com>
2015-11-19 10:39:14 +0700

    Fix associated of user - role

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ module.exports = {
     associate : function (models) {
         models.menus.hasMany(models.menu_detail,{ foreignKey: 'id'});
         models.menu_detail.belongsTo(models.menus,{ foreignKey: 'menu_id'});
-        models.user.hasMany(models.role,{foreignKey : 'role_id'});
-        models.role.belongsTo(models.user,{foreignKey : "role_id"})
+        models.user.belongsTo(models.role, {foreignKey: 'role_id'});
+        models.role.hasMany(models.user, {foreignKey: 'role_id'});
 
     }
 };
