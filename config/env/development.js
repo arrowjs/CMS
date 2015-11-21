e6365d3224153d03065e557515f579e0380d7a76
thanhvk <thanhvk.w3s@gmail.com>
2015-11-21 17:29:58 +0700

    Before pull CMS (robinhuy edit)

--- config/env/development.js
+++ config/env/development.js
@@ -5,9 +5,9 @@ module.exports = {
     db: {
         host: 'localhost',
         port: '5432',
-        database: 'arrowjs',
-        username: 'postgres',
-        password: '',
+        database: 'arrowjs_cms',
+        username: 'thanhvk',
+        password: '310928@',
         dialect: 'postgres',
         logging: false
     },
