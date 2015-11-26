8c5290d9847088c56b2ddc361583cc02d4068132
robinhuy <robin@techmaster.vn>
2015-11-26 09:58:42 +0700

    reformat code

--- widgets/categories/controllers/index.js
+++ widgets/categories/controllers/index.js
@@ -20,17 +20,17 @@ module.exports = function (controller, component, application) {
     controller.renderWidget = function (widget) {
         // Get layouts
         let layout;
-        try{
-            layout=JSON.parse(widget.data).layout;
-        }catch(err){
-            layout=component.getLayouts(widget.widget_name)[0];
+        try {
+            layout = JSON.parse(widget.data).layout;
+        } catch (err) {
+            layout = component.getLayouts(widget.widget_name)[0];
         }
 
         // Get all categories
         return application.models.category.findAll({
             raw: true,
             limit: JSON.parse(widget.data).number_of_categories
-        }).then(function(categories){
+        }).then(function (categories) {
             // Render view with layout
             return component.render(layout, {
                 widget: JSON.parse(widget.data),
