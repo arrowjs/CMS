a6bff7bc64f0bf9edc677457e8f9d2742c44c026
robinhuy <robin@techmaster.vn>
2015-11-26 16:41:39 +0700

    add development.js

---
+++ config/env/development.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    /**
+     * Uncomment to override config for development environment
+     */
+    //db: {
+    //    host: 'localhost',
+    //    port: '5432',
+    //    database: 'arrowjs',
+    //    username: 'postgres',
+    //    password: '',
+    //    dialect: 'postgres',
+    //    logging: false
+    //},
+    //redis: {
+    //    host: 'localhost',
+    //    port: '6379'
+    //}
+};
