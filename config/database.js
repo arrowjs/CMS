ace3164c4cccc3c67fce689a54a7a4ca4ad70815
robinhuy <robin@techmaster.vn>
2015-11-26 16:15:08 +0700

    change default config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,20 @@
 "use strict";
 
 module.exports = {
+    db: {
+        host: 'localhost',
+        port: '5432',
+        database: 'arrowjs',
+        username: 'postgres',
+        password: '',
+        dialect: 'postgres',
+        logging: false
+    },
     associate: function (models) {
         models.menu.hasMany(models.menu_detail, {foreignKey: 'menu_id'});
         models.menu_detail.belongsTo(models.menu, {foreignKey: 'menu_id'});
         models.user.belongsTo(models.role, {foreignKey: 'role_id'});
         models.role.hasMany(models.user, {foreignKey: 'role_id'});
-
         models.post.belongsTo(models.user, {foreignKey: "created_by"})
     }
 };
