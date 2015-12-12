0fd4154bf08af1c1b686cab43eca4c8dbaf70915
Nguyễn Văn Thắng <thangtyt@gmail.com>
2015-12-12 14:46:00 +0700

    fix to check multi permission in isAllow function

--- library/js_utilities/helper/acl.js
+++ library/js_utilities/helper/acl.js
@@ -2,31 +2,42 @@
  * Created by thangnv on 11/14/15.
  */
 'use strict';
-
+let _ = require('arrowjs')._;
 /*
-* @req : request from feature
-* @permission : permission for check
-* function return true or false
-* */
+ * @req : request from feature
+ * @permission : permission for check
+ * function return true or false
+ * */
 exports.isAllow = function (req,permission) {
+    //get all permissions of current user
     let permissions = req.session.permissions;
+    //get key of current feature
     let featureName =  req.arrowUrl.split('.').pop();
+    let result = false;
+    /*
+     * check option of server.js
+     * if role == false => return true (allows all permissions)
+     * */
     if(!this.arrowSettings.role){
         return true;
     }
-    if (typeof permissions === 'object'){
-        if (permissions.hasOwnProperty('feature')){
-            for (let k in permissions.feature){
+    //check permission is object (JSON)
+    if (_.isObject(permissions)){
+        if (_.has(permissions,'feature')){
+            _.map(permissions.feature, function (v,k) {
                 if (k === featureName){
-                    for(let val of permissions.feature[k]){
-                        if (val.name === permission){
-                            return true;
-                        }
-                    }
+                    permission = (typeof permission === 'string')?[permission]:permission;
+                    _.map(permission, function (val) {
+                        _.map(v, function (value) {
+                            if(value['name']==val){
+                                result = true;
+                            }
+                        })
+                    })
                 }
-            }
+            })
         }
     }
-    return false;
+    return result;
 };
 
