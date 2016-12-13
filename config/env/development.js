fcd712714860b32e877d60cee137d420278eed0d
Le Phu Hai <lephuhai0511@gmail.com>
2016-12-13 11:00:32 +0700

    Refactor cms features theme

--- config/env/development.js
+++ config/env/development.js
@@ -4,18 +4,18 @@ module.exports = {
     /**
      * Uncomment to override config in development environment
      */
-    //port: process.env.PORT || 8000,
-    //db: {
-    //    host: 'localhost',
-    //    port: '5432',
-    //    database: 'arrowjs',
-    //    username: 'postgres',
-    //    password: '',
-    //    dialect: 'postgres',
-    //    logging: false
-    //},
-    //redis: {
-    //    host: 'localhost',
-    //    port: '6379'
-    //}
+    port: process.env.PORT || 8000,
+    db: {
+       host: 'localhost',
+       port: '5432',
+       database: 'arrowjs',
+       username: 'postgres',
+       password: '123456',
+       dialect: 'postgres',
+       logging: false
+    },
+    redis: {
+       host: 'localhost',
+       port: '6379'
+    }
 };
